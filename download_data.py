fe0453d4fe5a304c39a184bf0189b9e010dff1ba
Glen Rutherford <glenrutherford4@gmail.com>
2025-01-10 15:31:45 +1100

    Saving image to file done

--- download_data.py
+++ download_data.py
@@ -1,14 +1,17 @@
 import yfinance as yf
-import pandas as pd
+import os
 
 # Variables
 stockSymbol = "GC=F"    # Gold futures
 startDate = '2024-01-01'
 endDate = '2024-12-31'
 
+# Create sub-folders
+if not os.path.exists("data"):
+    os.mkdir("data")
+
 # Download price data 
 stock_data = yf.download(stockSymbol, start='2024-01-01', end='2024-12-31')
 
 # Save the data to a CSV file
-# TODO create data folder if it doesn't exist
 stock_data.to_csv('./data/{}_{}_{}.csv'.format(stockSymbol, startDate, endDate))
